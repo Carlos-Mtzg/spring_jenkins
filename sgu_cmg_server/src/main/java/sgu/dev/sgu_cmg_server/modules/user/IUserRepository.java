9c64fdc945f7dfd961c9bae87cd2d942258620e5
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-10-22 15:20:57 -0600

    feat: add methods to find UserModel by email and phone in IUserRepository

--- sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/modules/user/IUserRepository.java
+++ sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/modules/user/IUserRepository.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface IUserRepository extends JpaRepository<UserModel, Long> {
     Optional<UserModel> findByUuid(UUID uuid);
+
+    Optional<UserModel> findByEmail(String email);
+
+    Optional<UserModel> findByPhone(String phone);
 }
