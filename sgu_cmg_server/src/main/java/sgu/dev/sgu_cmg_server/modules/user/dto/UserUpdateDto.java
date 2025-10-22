f06b49d6210c3dab297878bae83b411a33a31892
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-10-22 15:21:43 -0600

    feat: add UserUpdateDto for user update operations with validation

--- sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/modules/user/dto/UserUpdateDto.java
+++ sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/modules/user/dto/UserUpdateDto.java
@@ -1,34 +1,31 @@
 package sgu.dev.sgu_cmg_server.modules.user.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserRequestDto {
+public class UserUpdateDto {
 
     public static final String NO_ANGLE_BRACKETS_MESSAGE = "No se permiten los caracteres < o >";
     public static final String NO_ANGLE_BRACKETS_REGEX = "^[^<>]*$";
-    public static final String NOT_NULL_MESSAGE = "Este campo es obligatorio";
 
     public static final String PHONE_MESSAGE = "Teléfono inválido. Sólo dígitos (opcional '+' al inicio), 7-15 caracteres, sin < ni >";
     public static final String PHONE_REGEX = "^(?!.*[<>])[+]?[0-9]{7,15}$";
 
-    @NotNull(message = NOT_NULL_MESSAGE)
     @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String fullName;
 
     @Email(message = "El formato del correo electrónico es inválido")
-    @NotNull(message = NOT_NULL_MESSAGE)
     @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String email;
 
-    @NotNull(message = NOT_NULL_MESSAGE)
     @Pattern(regexp = PHONE_REGEX, message = PHONE_MESSAGE)
     private String phone;
 
