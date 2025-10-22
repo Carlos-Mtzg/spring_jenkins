fc1533f21199c481a5e0b47c5aaa70d31c994270
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-10-22 14:28:08 -0600

    feat: add initial Spring Boot application structure with Maven wrapper and configuration files

---
+++ sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/SguCmgServerApplication.java
@@ -0,0 +1,13 @@
+package sgu.dev.sgu_cmg_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SguCmgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SguCmgServerApplication.class, args);
+	}
+
+}
---
+++ sgu_cmg_server/src/test/java/sgu/dev/sgu_cmg_server/SguCmgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sgu.dev.sgu_cmg_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SguCmgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
