91de826032456569666bf0aeda574c5ce6acdb42
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-10-22 14:46:09 -0600

    feat: add IUserRepository interface for user data access

---
+++ sgu_cmg_server/src/main/java/sgu/dev/sgu_cmg_server/modules/user/IUserRepository.java
@@ -0,0 +1,12 @@
+package sgu.dev.sgu_cmg_server.modules.user;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
+    Optional<UserModel> findByUuid(UUID uuid);
+}
